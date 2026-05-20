db5bd98f2d2e3e0655786b35ecd221aafee1a6c4
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-05-19 22:50:51 -0600

    feat: add models

---
+++ UsuariosAPI/Models/ApplicationRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UsuariosAPI.Models;
+public class ApplicationRole : IdentityRole
+{
+    public bool Estado { get; set; } = true;
+    public ApplicationRole() : base() { }
+    public ApplicationRole(string roleName) : base(roleName) { }
+
+}
---
+++ UsuariosAPI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UsuariosAPI.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string Nombre { get; set; } = string.Empty;
+    public bool Estado { get; set; } = true;
+}
