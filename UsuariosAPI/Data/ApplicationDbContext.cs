6adcdfbe521771118412ba5f603ef5d25f61441b
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-05-22 21:14:06 -0600

    feat: add context

---
+++ UsuariosAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
