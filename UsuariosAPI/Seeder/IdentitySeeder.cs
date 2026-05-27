7bf6e00dae5a2d56220fa1291b8c4a83a2bea427
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-05-26 19:56:34 -0600

    feat: add IdentitySeeder

---
+++ UsuariosAPI/Seeder/IdentitySeeder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Seeder;
+public class IdentitySeeder
+{
+    public static async Task SeedAsync(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        var roles = new[] { "Admin", "Seller", "Manager" };
+
+        foreach(var role in roles)
+        {
+            if(!await roleManager.RoleExistsAsync(role))
+            {
+                var newRole = new ApplicationRole(role);
+                await roleManager.CreateAsync(newRole);
+            }
+        }
+
+        var adminEmail = "administrador@mail.com";
+        var password = "Admin123@";
+
+        var existingAdmin = await userManager.FindByEmailAsync(adminEmail);
+
+        if(existingAdmin == null)
+        {
+            var adminUser = new ApplicationUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+                Estado = true
+            };
+        }
+           
+    }
+}
