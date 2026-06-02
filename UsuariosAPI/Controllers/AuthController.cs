f36296b5cd8c9cb3ef612300b553afb4cdd9a00a
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-06-01 20:21:55 -0600

    feat> authController

--- UsuariosAPI/Controllers/AuthController.cs
+++ UsuariosAPI/Controllers/AuthController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using UsuariosAPI.DTOs;
 using UsuariosAPI.Models;
 
@@ -45,15 +49,38 @@ public class AuthController : ControllerBase
 
         var roles = await _userManager.GetRolesAsync(user);
 
-        foreach(var roleName in roles)
+        foreach (var roleName in roles)
         {
-            var role  = await _roleManager.FindByIdAsync(roleName);
+            var role = await _roleManager.FindByNameAsync(roleName);
 
             if (role is null || !role.Estado)
-                return Unauthorized($"Su rol {roleName} está inactivo");
+                return Unauthorized("Credenciales inválidas");
         }
 
+        var claims = new List<Claim> {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id)
+        };
 
-        return Ok();
+        foreach (var role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: credentials
+        );
+
+        return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            }
+        );
     }
 }
