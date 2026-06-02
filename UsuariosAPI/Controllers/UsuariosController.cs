9ebc0ba05ef8ee5a1c907d0588bd3d1911664a2e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-06-01 21:02:06 -0600

    feat: UsuariosController

---
+++ UsuariosAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class UsuariosController : ControllerBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<ApplicationRole> _roleManager;
+
+    public UsuariosController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+
+}
