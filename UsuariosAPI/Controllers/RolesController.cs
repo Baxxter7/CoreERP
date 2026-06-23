860f413a5cd4bb77fecb83d1043f0042b226061c
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-06-23 13:43:38 -0600

    feat: add RolesController

---
+++ UsuariosAPI/Controllers/RolesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public RolesController(RoleManager<ApplicationRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+
+    }
+}
