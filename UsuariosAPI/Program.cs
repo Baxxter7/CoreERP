656c85e0f9cc98f9ebbc443e3a44c40c22ca0fbd
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-06-26 21:02:31 -0600

    refactor: config program cs

--- UsuariosAPI/Program.cs
+++ UsuariosAPI/Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "UsuariosAPI", Version = "1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT en header. Ej: Bearer {token}",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+
+});
 
 var app = builder.Build();
 
