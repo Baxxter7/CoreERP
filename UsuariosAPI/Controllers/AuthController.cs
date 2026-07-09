ca9f63516f0618b6cbacfee39e73c84c67c63410
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-07-08 22:25:41 -0600

    refactor: agregando ! para notificar que ese valor siempre estara lleno, para no tener warning de nullabilidad

--- UsuariosAPI/Controllers/AuthController.cs
+++ UsuariosAPI/Controllers/AuthController.cs
@@ -71,7 +71,7 @@ public class AuthController : ControllerBase
         foreach (var role in roles)
             claims.Add(new Claim(ClaimTypes.Role, role));
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
 
         var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
