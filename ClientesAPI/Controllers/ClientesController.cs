349c13ed9cb8b82cd3b6454ab698366cdc8cf87a
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-07-14 23:42:09 -0600

    feat: add ClientesController

---
+++ ClientesAPI/Controllers/ClientesController.cs
@@ -0,0 +1,38 @@
+﻿using ClientesAPI.Data;
+using ClientesAPI.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ClientesDbContext _context;
+
+        public ClientesController(ClientesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClienteDto>>> GetClientes()
+        {
+            var clientesDb = await _context.Clientes.ToListAsync();
+
+            var listaClientes = clientesDb.Select(c => new ClienteDto
+            {
+                Id = c.Id,
+                Nombre = c.Nombre,
+                Telefono = c.Telefono,
+                Direccion = c.Direccion
+            }).ToList();
+
+
+            return Ok(listaClientes);
+        }
+    }
+}
