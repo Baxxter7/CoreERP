c2a9110d5f67d59fe8e3036d1be793307b1faa19
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-14 14:13:04 -0600

    feat: add dto y dbContext

---
+++ ProveedoresAPI/Data/ProveedoresDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProveedoresAPI.Models;
+
+namespace ProveedoresAPI.Data
+{
+    public class ProveedoresDbContext : DbContext
+    {
+        public ProveedoresDbContext(DbContextOptions<ProveedoresDbContext> options) : base(options) { }
+
+        public DbSet<Proveedor> Proveedores => Set<Proveedor>();
+    }
+}
