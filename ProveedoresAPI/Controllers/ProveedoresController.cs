3d292f287a0bd399659518f81739f10dc641cc8e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-14 14:13:25 -0600

    feat: add obtener todos los proveedores

---
+++ ProveedoresAPI/Controllers/ProveedoresController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProveedoresAPI.Data;
+using ProveedoresAPI.DTOs;
+
+namespace ProveedoresAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProveedoresController : ControllerBase
+    {
+        private readonly ProveedoresDbContext _context;
+        public ProveedoresController(ProveedoresDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProveedorDto>>> GetAll()
+        {
+            var proveedores = await _context.Proveedores
+                .AsNoTracking()
+                .Select(p => new ProveedorDto
+                {
+                    Id = p.Id,
+                    Nombre = p.Nombre,
+                    Telefono = p.Telefono,
+                    Direccion = p.Direccion,
+                    Estado = p.Estado,
+                })
+                .ToListAsync();
+
+            return Ok(proveedores);
+        }
+
+
+    }
+}
