411b8cc4aa75ef7c7ba9dbf445d6c7a2c264fe71
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-14 14:12:43 -0600

    feat: add model

---
+++ ProveedoresAPI/Models/Proveedor.cs
@@ -0,0 +1,11 @@
+﻿namespace ProveedoresAPI.Models
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Direccion { get; set; } = string.Empty;
+        public bool Estado { get; set; }
+    }
+}
