ed28f5a1d9a6cf4390a841efc3c5933daf616e0c
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-15 21:59:06 -0600

    feat: add ComprasDbContext

---
+++ ComprasAPI/Data/ComprasDbContext.cs
@@ -0,0 +1,33 @@
+﻿using ComprasAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComprasAPI.Data
+{
+    public class ComprasDbContext : DbContext
+    {
+        public ComprasDbContext(DbContextOptions<ComprasDbContext> options) : base(options) { }
+
+        public DbSet<OrdenCompra> OrdenCompras { get; set; }
+        public DbSet<DetalleOrden> DetalleOrdenes { get; set; }
+        public DbSet<Recepcion> Recepciones { get; set; }
+        public DbSet<DetalleRecepcion> DetalleRecepciones { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<OrdenCompra>()
+                .HasMany(c => c.Detalles)
+                .WithOne()
+                .HasForeignKey(d => d.OrdenCompraId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder
+                .Entity<Recepcion>()
+                .HasMany(r => r.Detalles)
+                .WithOne()
+                .HasForeignKey(d => d.RecepcionId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
