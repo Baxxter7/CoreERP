5567b5ab9a52482053199a39c584c894c8809d02
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-16 19:34:15 -0600

    feat: configuraciones para comunicarse con servicio de inventario y propagacion del JWT

---
+++ ComprasAPI/Data/InventoryServiceClient.cs
@@ -0,0 +1,25 @@
+﻿using ComprasAPI.DTOs;
+
+namespace ComprasAPI.Data
+{
+    public class InventoryServiceClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public InventoryServiceClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task RegistrarMovimientoYactualizarStockAsync(MovimientoDto movimientoDto)
+        {
+            var response = await _httpClient.PostAsJsonAsync(
+                "api/stock/registrar-movimiento",
+                movimientoDto
+            );
+
+            response.EnsureSuccessStatusCode();
+        }
+
+    }
+}
