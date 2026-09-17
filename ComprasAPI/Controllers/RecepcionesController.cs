c8256e96e3b4837d6fc6a3c9b5f08130443d91d1
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-17 10:50:32 -0600

    feat: obtener todas las recepciones

---
+++ ComprasAPI/Controllers/RecepcionesController.cs
@@ -0,0 +1,51 @@
+﻿using ComprasAPI.Data;
+using ComprasAPI.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComprasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RecepcionesController : ControllerBase
+    {
+        private readonly ComprasDbContext _dbContext;
+        private readonly InventoryServiceClient _inventoryServiceClient;
+
+        public RecepcionesController(ComprasDbContext dbContext, InventoryServiceClient inventoryServiceClient)
+        {
+            _dbContext = dbContext;
+            _inventoryServiceClient = inventoryServiceClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RecepcionDto>>> GetRecepciones()
+        {
+            var recepciones = await _dbContext.Recepciones
+                .AsNoTracking()
+                //.Include(r => r.Detalles)
+                .Select(r => new RecepcionDto
+                {
+                    Id = r.Id,
+                    OrdenCompraId = r.OrdenCompraId,
+                    SucursalId = r.SucursalId,
+                    Fecha = r.Fecha,
+                    Observaciones = r.Observaciones,
+                    Detalles =
+                        r.Detalles.Select(d => new DetalleRecepcionDto
+                        {
+                            Id = d.Id,
+                            RecepcionId = d.RecepcionId,
+                            ProductoId = d.ProductoId,
+                            CantidadRecibida = d.CantidadRecibida
+                        }).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(recepciones);
+        }
+
+    }
+}
