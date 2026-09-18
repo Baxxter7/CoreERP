c932b46bdf86e5b992137f9881183235aa380374
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-17 19:28:08 -0600

    refactor: quitar depencias que no se usan

--- ProveedoresAPI/Program.cs
+++ ProveedoresAPI/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using ProveedoresAPI.Controllers;
 using ProveedoresAPI.Data;
 using System.Text;
 
