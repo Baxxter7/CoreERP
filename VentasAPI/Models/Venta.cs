00491a465ad7371930f6b12a20887fc8f4725d4b
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-21 23:00:57 -0600

    feat: agregar modelos de ventas

---
+++ VentasAPI/Models/Venta.cs
@@ -0,0 +1,12 @@
+﻿namespace VentasAPI.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public int SucursalId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Total { get; set; }
+        public List<DetalleVenta> Detalles { get; set; } = new();
+    }
+}
