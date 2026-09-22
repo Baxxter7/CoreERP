462c20494fb95fff9511e1e51e9837be8a23049d
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-21 23:01:15 -0600

    feat: agregar Dto

---
+++ VentasAPI/DTOs/DetalleVentaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace VentasAPI.DTOs
+{
+    public class DetalleVentaDto
+    {
+        public int Id { get; set; }
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
---
+++ VentasAPI/DTOs/VentaDto.cs
@@ -0,0 +1,12 @@
+﻿namespace VentasAPI.DTOs
+{
+    public class VentaDto
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public int SucursalId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Total { get; set; }
+        public List<DetalleVentaDto>? Detalles { get; set; }
+    }
+}
