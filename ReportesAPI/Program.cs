f00981b97feff8db88f65d27ccfe65d1d93bb52f
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-09-23 18:48:22 -0600

    feat: configurando el program

--- ReportesAPI/Program.cs
+++ ReportesAPI/Program.cs
@@ -1,9 +1,102 @@
+using ClientesAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using QuestPDF;
+using ReportesAPI.Interfaces;
+using ReportesAPI.Services;
+using System.Text;
+
+Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<PropagateJwtHandler>();
+
+string? jwtKey = builder.Configuration["Jwt:Key"];
+if (string.IsNullOrEmpty(jwtKey))
+    throw new Exception("Jwt:key no configurado. Válida para poder continuar");
+
+var key = Encoding.UTF8.GetBytes(jwtKey);
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "ReportesAPI", Version = "v1" });
 
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT en header. Ej: Bearer {token}",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+
+                Array.Empty<string>()
+            }
+        }
+    );
+
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+
+//Clientes API
+var clientesApi = builder.Configuration["ClientesApiUrl"] 
+                            ?? throw new ArgumentException("ClientesApiUrl no esta configurada");
+
+builder
+    .Services.AddHttpClient<IClienteService, ClienteService>(client =>
+    {
+        client.BaseAddress = new Uri(clientesApi);
+    })
+    .AddHttpMessageHandler<PropagateJwtHandler>();
+
+//Clientes API
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,5 +107,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
